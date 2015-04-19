54d2877ddd9fe31009a6c65f9b6a780234150320
peterjacobson <peterjacobson.nz@gmail.com>
2015-04-19 14:16:55 +1200

    page cleaned, messages hidden at sides, compass set up compiling sass, jquery head animation working

--- js/app.js
+++ js/app.js
@@ -1,26 +1,59 @@
+// MVP ===============================================================
+
+// ANIMATIONS -------------------------------------------------------------
+
+// on window load, animate face slide down to forehead height
+$(window).load(function() {
+  // $('#peteFace').animate({margin-top: -100%}, 1000)
+  $('header').animate({margin: '-50% 0 0 0'}, 1000)
+});
+
+// on forehead click,
+$('#peteFace').click()
+  // face slides all the way down
+
+  // first pete message slides in from left
+
+  // second pete message slides in from left
+
+  // first user message slides up from bottom
+
+// on click of user message option button
+
+  // ----- scroll down page
+
+  //
+
+
+// INTERACTIONS -----------------------------------------------------------
+
 //Problem: Navigation not interactive
 //Solution: Make navigation beautiful using jQuery for sliding menu bars etc
-
 // var $navButton = ;
-
 //On nav-button click slide out primary menu with animation
 
-$('#menu-toggle').click({element: 'nav'}, toggleCollapse);
-$('#portfolio-toggle').click({element: '#portfolio-nav'}, toggleCollapse);
-
-
-
-function toggleCollapse(event) {
-  console.log(event.data);
-  if ($(event.data.element).css("height") === "0px") {
-    console.log('expand');
-    $(event.data.element).css('height', 'initial');
-  } else {
-    console.log('collapse');
-    $(event.data.element).css('height', "0px");
-  }
-}
+// $('#menu-toggle').click({element: 'nav'}, toggleCollapse);
+// $('#portfolio-toggle').click({element: '#portfolio-nav'}, toggleCollapse);
+//
+// function toggleCollapse(event) {
+//   console.log(event.data);
+//   if ($(event.data.element).css("height") === "0px") {
+//     console.log('expand');
+//     $(event.data.element).css('height', 'initial');
+//   } else {
+//     console.log('collapse');
+//     $(event.data.element).css('height', "0px");
+//   }
+// }
 
 // On click to header or away from nav, hide nav
-
 //On portfolio click expand collapsed sub-menu
+
+
+
+
+// ADDITIONAL FEATURES ======================================================
+
+// on another face click
+
+  // message pop in above head - "ow, stop poking me"
