8e62b23a7d2b756321413f33879bb164e0bac12a
peterjacobson <peterjacobson.nz@gmail.com>
2015-04-19 13:20:59 +1200

    pseudocode for animation flow

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
+// MVP ------------------------------------------------
+
+// on window load, animate face slide down to forehead height
 $(window).load(function() {
-  $('header').addClass("animate-down")
+  $('header').
 });
 
+// on forehead click,
 $('#peteFace').click()
+  // face slides all the way down
+
+  // first pete message slides in from left
+
+  // second pete message slides in from left
+
+  // first user message slides up from bottom
+
+// on click of user message option button
+
+  // ----- scroll down page
+
+  //
+
+
+
+
+
+
+
+
+// ADDITIONAL FEATURES --------------------------------
+// on another face click
+
+  // message pop in above head - "ow, stop poking me"
